ebeab9bb8694000f378cff8b1095645d00bd45aa
ashevchuk <dev.ashevchuk@gmail.com>
2015-03-18 06:50:58 +0000

    added config deployment

---
+++ deploy/plan/make_config/0-install.sh
@@ -0,0 +1,8 @@
+mkdir -p /home/developer/devel/perl/Slaver/etc/configdb
+mkdir -p /home/developer/devel/perl/Slaver/var/log/mongodb
+mkdir -p /home/developer/devel/perl/Slaver/var/data/configdb
+mkdir -p /home/developer/devel/perl/Slaver/var/run
+mkdir -p /home/developer/devel/perl/Slaver/etc/single/init.d
+{UPLOAD}/home/developer/devel/perl/Slaver/etc/configdb/mongod.conf=>/home/developer/devel/perl/Slaver/etc/configdb/mongod.conf
+{UPLOAD}/home/developer/devel/perl/Slaver/etc/single/init.d/configdb.start=>/home/developer/devel/perl/Slaver/etc/single/init.d/configdb.start
+{UPLOAD}/home/developer/devel/perl/Slaver/etc/single/init.d/configdb.stop=>/home/developer/devel/perl/Slaver/etc/single/init.d/configdb.stop
