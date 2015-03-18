bb6596565ff26c1ecd4d07a140a528ad51a6e222
ashevchuk <dev.ashevchuk@gmail.com>
2015-03-18 15:42:57 +0000

    updated deployment plan

--- deploy/plan/install_packages/0-install.sh
+++ deploy/plan/install_packages/0-install.sh
@@ -1,1 +1,7 @@
-apt-get -y install libevent-dev
+#apt-get -y install libevent-dev
+apt-get install -y -q liblcms2-dev
+apt-get install -y -q liblcms2-2
+#cd /home/developer/devel/perl/Slaver/var/tmp/upload_store
+#./mkdir
+#cd /home/developer/devel/perl/Slaver/var/tmp/upload_state
+#./mkdir
