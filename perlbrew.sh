cfe543e1cdb913bdd8fc3161668bb4d94bc0dfaf
User Developer <developer@www.lambda-cloud.net>
2015-06-09 21:07:13 +0000

    updated perlbrew.sh

--- perlbrew.sh
+++ perlbrew.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#wget --no-check-certificate -O - http://install.perlbrew.pl | bash
-#echo "source ~/perl5/perlbrew/etc/bashrc" >> ~/.bash_profile
+wget --no-check-certificate -O - http://install.perlbrew.pl | bash
+echo "source ~/perl5/perlbrew/etc/bashrc" >> ~/.bash_profile
 
-#cd ~/
+cd ~/
 
-#. .bashrc
+. .bashrc
 
 perlbrew install-cpanm
-perlbrew install --force --thread --multi --64int --64all --ld --clang --notest -Accflags='-fPIC' perl-5.20.2 -D useshrplib
-perlbrew switch perl-5.20.2
+perlbrew install --force --thread --multi --64int --64all --ld --clang --notest -Accflags='-fPIC' perl-5.22.0 -D useshrplib
+perlbrew switch perl-5.22.0
